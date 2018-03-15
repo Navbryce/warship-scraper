5c586b796d32c71348beba76589b2adbb4fd3d53
Bryce Plunkett <irvingwashington01@gmail.com>
2018-03-14 21:04:32 -0400

    Added date comparison tests. Changed to hashmap for the way dates are stored in the ships from the boatscraping script.

--- ship_compare/ship_compare.py
+++ ship_compare/ship_compare.py
@@ -1,3 +1,4 @@
+import json
 import sys
 sys.path.insert(0, "../..")
 from ABoatScraping.Calculate import Calculate
@@ -10,8 +11,10 @@ class ShipCompare(object):
         self.shipTwo = shipTwo
         self.edges = {} # A dictionary where each property is an edge + "+" + source combination
 
+        self.runDateComparisons()
 
-    def addLink(self, source, target, magnitude, reasons):
+
+    def addEdge(self, source, target, magnitude, reasons): # heka unecessary right now since this class only compares two ships. Will remove in future. Will be added to a higher level class that creates instances of ShipCompare
         """
         reasons should an array of strings
         checks to see if edge already exists
@@ -20,7 +23,8 @@ class ShipCompare(object):
         if key in self.edges: # the edge is already in the array
             existingEdge = self.edges[key]
             existingEdge.incrementMagnitude(magnitude, reasons)
-
+        else:
+            self.createEdge(source, target, magnitude, reasons)
     def createEdge(self, source, target, magnitude, reasons):
         """
         creates edges
@@ -34,12 +38,33 @@ class ShipCompare(object):
         return source + "+" + target
 
     def getEdgesBetweenShips(self):
-        """ Returns the edges between ships in array. Each object of the array is an Edge object"""
-        return self.edges
+        """ Returns the seraliazed edges between ships in array. Each object of the array is an Edge object"""
+        serializedEdges = []
+        for edgeKey in self.edges:
+            serializedEdges.append(self.edges[edgeKey].toSerializableForm())
+        return serializedEdges
+
+    def runDateComparisons(self):
+        """runs the comparison tests involved with dates. only run date comparison tests for types of dates all ships have in order to keep the edges balanced"""
+        datesToCheckFor = ['laid down', 'launched', 'commissioned']
+        shipOneDates = self.shipOne["importantDates"]
+        shipTwoDates = self.shipTwo["importantDates"]
+        for dateToCheckFor in datesToCheckFor:
+            shipOneHasDate = dateToCheckFor in shipOneDates
+            shipTwoHasDate = dateToCheckFor in shipTwoDates
+
+            if shipOneHasDate and shipTwoHasDate:
+                if self.compareDate(shipOneDates[dateToCheckFor], shipTwoDates[dateToCheckFor]): # they are within tolerances
+                    self.addEdge(self.shipOne["scrapeURL"], self.shipTwo["scrapeURL"], 1, ["Within tolerances of date for " + dateToCheckFor])
+            else:
+                if not shipOneHasDate:
+                    print(self.shipOne["displayName"] + " does not have a date for " + dateToCheckFor)
+                if not shipTwoHasDate:
+                    print(self.shipTwo["displayName"] + " does not have a date for " + dateToCheckFor)
 
     #Compare functions
     def compareDate(self, date_one_object, date_two_obect):
-        return Calculate.withinRange(date_one_object.date.year, date_two_obect.date.year, 5) # if they were made within a + 5 year, -5 year range
+        return Calculate.withinRange(date_one_object["year"], date_two_obect["year"], 5) # if they were made within a + 5 year, -5 year range
 
     def compareValue(self, value_one, value_two):
         """compares values within the standard tolerance. certain values might want a custom tolerance. this just provides a standard method"""
@@ -51,4 +76,4 @@ ship_one = boatDatabase.findShips(filter)[0]
 filter = {"scrapeURL": "https://en.wikipedia.org/wiki/German_battleship_Gneisenau"}
 ship_two = boatDatabase.findShips(filter)[0]
 shipCompareTest = ShipCompare(ship_one, ship_two)
-print(shipCompareTest.edges)
+print(json.dumps(shipCompareTest.getEdgesBetweenShips()))
