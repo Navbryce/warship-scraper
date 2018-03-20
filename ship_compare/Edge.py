55a95544f4185c522b8d0e99ce410b251b56368c
Bryce Plunkett <irvingwashington01@gmail.com>
2018-03-19 21:07:55 -0400

    Improved the way edge id's are calculated. Id's are not dependent on which url is the target and which is the source. They output the same value. Kidna proud of this solution

--- ship_compare/Edge.py
+++ ship_compare/Edge.py
@@ -1,3 +1,5 @@
+import hashlib
+
 class Edge(object):
     def __init__(self, source, target, intialMagnitude = 1, reasons = ["No reason provided"]):
         """ reasons should be an array of strings """
@@ -9,7 +11,13 @@ class Edge(object):
 
     def generateID(self, source, target):
         """generates a 'propertykey'. Essentially a key in the edges map (AKA python dictionary) """
-        return source + "+" + target
+        sourceHash = self.getHashForString(source)
+        targetHash = self.getHashForString(target)
+        print(sourceHash)
+        return sourceHash*targetHash # ensures a unique value for each source, target combination THAT IS NOT dependent on which is source and which is hash. 
+
+    def getHashForString(self, string):
+        return int(hashlib.sha256(string.encode('utf-8')).hexdigest(), 16) % 10 ** 8 # shorten to 8 digit number because that's the max allowable by mongo
 
     def incrementMagnitude(self, magnitude_increment, reasons):
         """ reasons should be an array of strings """
